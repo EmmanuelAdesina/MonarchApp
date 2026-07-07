dd07458b29e7d7926fdd42a9c17e02a432a6ce6a
Emmanuel Adesina <aboidune704@gmail.com>
2026-07-07 18:24:58 +0100

    Use writable Flask instance path

--- main.py
+++ main.py
@@ -3311,4 +3311,4 @@ with app.app_context():
         print("Migration warning (approving pre-existing users):", err)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
